0e240b1a5b0681eca84f209dae8dfecfecfb8d69
NT350XCR-AD5AS <goo.gl.denver@gmail.com>
2023-09-24 13:27:19 +0900

    no message

---
+++ src/containers/baking/list/index.js
@@ -0,0 +1,77 @@
+import qs from 'qs';
+import React, { useEffect } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { bakingList, initialList } from '@/modules/baking/list';
+import List from '@/components/baking/list';
+
+const BakingList = ({ attributes }) => {
+  const { horizon, category } = attributes || {};
+
+  const { user, list, pagination, error, loading } = useSelector(
+    ({ user, bakingList, loading }) => ({
+      user: user.user?.user2,
+      list: bakingList.data?.list,
+      pagination: bakingList.data?.pagination,
+      error: bakingList.error,
+      loading: loading['baking/list']
+    }),
+    shallowEqual
+  );
+
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const prefixed = qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+
+  let number = location.pathname
+    .split('/')
+    .filter((element) => element !== null && element !== undefined && element !== '')
+    .splice(-1)[0];
+
+  const test = location.pathname
+    .split('/')
+    .filter((element) => element !== null && element !== undefined && element !== '')
+    .splice(-2)[0];
+
+  if (typeof number === 'undefined' || number === 'list' || number === 'read' || test === 'read') {
+    number = 1;
+  }
+
+  useEffect(() => {
+    dispatch(
+      bakingList({
+        category,
+        number,
+        select: prefixed.select,
+        keyword: prefixed.keyword
+      })
+    );
+
+    return () => {
+      console.log('board/list 언 마운트 될 때 리덕스에서 데이터를 삭제합니다.');
+
+      dispatch(initialList());
+    };
+  }, [dispatch, location.pathname, category, number, prefixed.select, prefixed.keyword]);
+
+  return (
+    <List
+      attributes={{
+        horizon,
+        category,
+        user,
+        list,
+        pagination,
+        error,
+        loading,
+        select: prefixed.select,
+        keyword: prefixed.keyword
+      }}
+    />
+  );
+};
+
+export default BakingList;
---
+++ src/containers/dessert/list/index.js
@@ -0,0 +1,77 @@
+import qs from 'qs';
+import React, { useEffect } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { dessertList, initialList } from '@/modules/dessert/list';
+import List from '@/components/baking/list';
+
+const BakingList = ({ attributes }) => {
+  const { horizon, category } = attributes || {};
+
+  const { user, list, pagination, error, loading } = useSelector(
+    ({ user, dessertList, loading }) => ({
+      user: user.user?.user2,
+      list: dessertList.data?.list,
+      pagination: dessertList.data?.pagination,
+      error: dessertList.error,
+      loading: loading['dessert/list']
+    }),
+    shallowEqual
+  );
+
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const prefixed = qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+
+  let number = location.pathname
+    .split('/')
+    .filter((element) => element !== null && element !== undefined && element !== '')
+    .splice(-1)[0];
+
+  const test = location.pathname
+    .split('/')
+    .filter((element) => element !== null && element !== undefined && element !== '')
+    .splice(-2)[0];
+
+  if (typeof number === 'undefined' || number === 'list' || number === 'read' || test === 'read') {
+    number = 1;
+  }
+
+  useEffect(() => {
+    dispatch(
+      dessertList({
+        category,
+        number,
+        select: prefixed.select,
+        keyword: prefixed.keyword
+      })
+    );
+
+    return () => {
+      console.log('board/list 언 마운트 될 때 리덕스에서 데이터를 삭제합니다.');
+
+      dispatch(initialList());
+    };
+  }, [dispatch, location.pathname, category, number, prefixed.select, prefixed.keyword]);
+
+  return (
+    <List
+      attributes={{
+        horizon,
+        category,
+        user,
+        list,
+        pagination,
+        error,
+        loading,
+        select: prefixed.select,
+        keyword: prefixed.keyword
+      }}
+    />
+  );
+};
+
+export default BakingList;
